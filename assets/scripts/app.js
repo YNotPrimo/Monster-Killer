a37d4f148efbb50366c664eac7786f2b8e3f7f4d
Primo <primo@pop-os.localdomain>
2020-08-18 11:25:48 +0300

    Reformatted the code

---
+++ assets/scripts/app.js
@@ -0,0 +1,42 @@
+const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
+const MONSTER_ATTACK_VALUE = 14;
+
+let chosenMaxLife = 100;
+let currentMonsterHealth = chosenMaxLife;
+let currentPlayerHealth = chosenMaxLife;
+
+adjustHealthBars(chosenMaxLife);
+
+function attackMonster(attackMode) {
+  let maxDamage;
+  if (attackMode === "ATTACK") {
+    maxDamage = ATTACK_VALUE;
+  } else if (attackMode === "STRONG ATTACK") {
+    maxDamage = STRONG_ATTACK_VALUE;
+  }
+  const damage = dealMonsterDamage(maxDamage);
+  currentMonsterHealth -= damage;
+
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDamage;
+
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert("You won!");
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert("You lost!");
+  } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+    alert("Draw!");
+  }
+}
+
+function attackHandler() {
+  attackMonster("ATTACK");
+}
+
+function strongAttackHandler() {
+  attackMonster("STRONG ATTACK");
+}
+
+attackBtn.addEventListener("click", attackHandler);
+strongAttackBtn.addEventListener("click", strongAttackHandler);
