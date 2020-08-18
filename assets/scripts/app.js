55a41d9c679ee247f5765c0e32de686ead51dcba
Primo <primo@pop-os.localdomain>
2020-08-18 13:15:44 +0300

    Refactoring

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -34,16 +34,15 @@ function writeToLog(event, value, monsterHealth, playerHealth) {
     finalMonsterHealth: monsterHealth,
     finalPlayerHealth: playerHealth,
   };
-  if (
-    event === LOG_EVENT_PLAYER_ATTACK ||
-    event === LOG_EVENT_PLAYER_STRONG_ATTACK
-  ) {
-    logEntry.target = "MONSTER";
-  } else if (
-    event === LOG_EVENT_MONSTER_ATTACK ||
-    event === LOG_EVENT_PLAYER_HEAL
-  ) {
-    logEntry.target = "PLAYER";
+  switch (event) {
+    case LOG_EVENT_PLAYER_ATTACK:
+    case LOG_EVENT_PLAYER_STRONG_ATTACK:
+      logEntry.target = "MONSTER";
+      break;
+    case LOG_EVENT_PLAYER_HEAL:
+    case LOG_EVENT_MONSTER_ATTACK:
+      logEntry.target = "PLAYER";
+      break;
   }
   battleLog.push(logEntry);
 }
